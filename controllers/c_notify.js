721889754903f8b604750df7cb3483d19c244463
#3 <931606284@qq.com>
2023-02-14 17:56:24 +0800

    chore:notify

--- controllers/c_notify.js
+++ controllers/c_notify.js
@@ -2,12 +2,13 @@ const Notify  = require('../model/notifyModel')
 const { verifyToken } = require('../dao/jwt')
 
 // 获取通知
-exports.getNotice = async (token,data,res) =>{
+exports.getNotice = async (token,res) =>{
     let tokenRes = verifyToken(token)
     let tokenNotify = await Notify.findOne({ userID:tokenRes.id})
     console.log(tokenNotify)
     if (tokenNotify && tokenNotify.notify_list.length > 0 ){
-        let result = await Notify.findOne({ userID: tokenRes.id }).populate("notify_list.opearUser")
-        res.send({status:200,result})
+        // let result = await Notify.findOne({ userID: tokenRes.id }).populate("notify_list.operaUser")
+        let result = await Notify.findOne({ userID: tokenRes.id }).populate("notify_list.operaUser")
+        // res.send({status:200,result})
     }
 }
--- routers/notify.js
+++ routers/notify.js
@@ -1,5 +1,3 @@
-const dbserver = require('../dao/dbserver')
-
 const { getNotice } = require('../controllers/c_notify')
 
 
@@ -7,6 +5,6 @@ module.exports = function(app) {
   app.get('/notify/notice',(req,res) => {
     let token = req.headers.authorization
     let data = req.query;
-    dbserver.getNotice(token,data,res)
+    getNotice(token,data,res)
   })
 }
